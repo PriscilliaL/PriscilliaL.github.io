f81de6093cf9740388b192eb01c6e389dc7abad2
PriscilliaL <lefebvre.priscillia60@gmail.com>
2017-04-11 13:35:27 +0200

    Ajout d'un menu burger en responsive et modifications du contenus

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,72 @@
+/*****************************
+-----------Boutons------------
+*****************************/
 $(document).ready(function() {
-    //Afficher/Masquer compétences
-    $('.liste_outils').hide(0);
-    $('#afficher_compt').click(function(){
-      $('.liste_outils').toggle(500);
-      $('.mes_experiences').hide();
-      $('.mes_formation').hide();
-      $('.mes_qualites').hide();
-      $('.mes_loisirs').hide();
-    })
-    //Afficher/Masquer experiences
-    $('.mes_experiences').hide(0);
-    $('#afficher_exp').click(function(){
-      $('.mes_experiences').toggle(500);
-      $('.mes_loisirs').hide();
-      $('.mes_formation').hide();
-      $('.mes_qualites').hide();
-      $('.liste_outils').hide();
-    })
-    //Afficher/Masquer formations
-    $('.mes_formation').hide(0);
-    $('#afficher_form').click(function(){
-      $('.mes_formation').toggle(500);
-      $('.liste_outils').hide();
-      $('.mes_experiences').hide();
-      $('.mes_qualites').hide();
-      $('.mes_loisirs').hide();
-    })
-    //Afficher/Masquer qualités
-    $('.mes_qualites').hide(0);
-    $('#afficher_qualt').click(function(){
-      $('.mes_qualites').toggle(500);
-      $('.liste_outils').hide();
-      $('.mes_experiences').hide();
-      $('.mes_loisirs').hide();
-      $('.mes_formation').hide();
-    })
+  //Afficher/Masquer compétences
+  $('.liste_outils').hide(0);
+  $('#afficher_compt').click(function(){
+    $('.liste_outils').toggle(500);
+    $('.mes_experiences').hide();
+    $('.mes_formation').hide();
+    $('.mes_qualites').hide();
   })
+  //Afficher/Masquer experiences
+  $('.mes_experiences').hide(0);
+  $('#afficher_exp').click(function(){
+    $('.mes_experiences').toggle(500);
+    $('.mes_formation').hide();
+    $('.mes_qualites').hide();
+    $('.liste_outils').hide();
+  })
+  //Afficher/Masquer formations
+  $('.mes_formation').hide(0);
+  $('#afficher_form').click(function(){
+    $('.mes_formation').toggle(500);
+    $('.liste_outils').hide();
+    $('.mes_experiences').hide();
+    $('.mes_qualites').hide();
+  })
+  //Afficher/Masquer qualités
+  $('.mes_qualites').hide(0);
+  $('#afficher_qualt').click(function(){
+    $('.mes_qualites').toggle(500);
+    $('.liste_outils').hide();
+    $('.mes_experiences').hide();
+    $('.mes_formation').hide();
+  })
+  /***************************
+  ----------Menu burger-------
+  ***************************/
+  //Afficher/Masquer compétences
+  $('.liste_outils').hide(0);
+  $('#burger_comp').click(function(){
+    $('.liste_outils').toggle(500);
+    $('.mes_experiences').hide();
+    $('.mes_formation').hide();
+    $('.mes_qualites').hide();
+  })
+  //Afficher/Masquer experiences
+  $('.mes_experiences').hide(0);
+  $('#burger_exp').click(function(){
+    $('.mes_experiences').toggle(500);
+    $('.mes_formation').hide();
+    $('.mes_qualites').hide();
+    $('.liste_outils').hide();
+  })
+  //Afficher/Masquer formations
+  $('.mes_formation').hide(0);
+  $('#burger_form').click(function(){
+    $('.mes_formation').toggle(500);
+    $('.liste_outils').hide();
+    $('.mes_experiences').hide();
+    $('.mes_qualites').hide();
+  })
+  //Afficher/Masquer qualités
+  $('.mes_qualites').hide(0);
+  $('#burger_qualt').click(function(){
+    $('.mes_qualites').toggle(500);
+    $('.liste_outils').hide();
+    $('.mes_experiences').hide();
+    $('.mes_formation').hide();
+  })
+})
